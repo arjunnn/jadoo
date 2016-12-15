e07f771df7b588299b671e1eea4a2b928da1d080
arjun <arjunmunji@gmail.com>
2016-12-15 13:44:38 +0530

    first commit :smile:

---
+++ js/crt.js
@@ -0,0 +1,25 @@
+function onLoad(arg) {
+    document.addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) {
+      toggleFullScreen();
+    }
+  });
+}
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.getElementById('monitor').webkitRequestFullscreen();
+
+    document.querySelector('#monitor').style.backgroundColor="#161913";
+    const nodisplays = document.querySelectorAll('.nodisplay');
+    nodisplays.forEach(nodisplay => nodisplay.classList.remove('nodisplay'));
+
+    document.querySelector('.enter-text').classList.add('nodisplay');
+    document.querySelector('.background-image').classList.add('nodisplay');
+
+    const promptKey = document.querySelector('.prompt-key');
+    promptKey.innerHTML = "B C F E";
+    promptKey.innerHTML += "\tB C E D";
+    promptKey.innerHTML += "\tB C F E D C"
+  }
+}
