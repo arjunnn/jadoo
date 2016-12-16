375e0d18ef4de9355aa2b2f403e57657b988b209
Arjun Munji <arjunmunji@gmail.com>
2016-12-16 16:00:33 +0530

    added README.md and better fullscreen function

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jadoo
+
+ An attempt to recreate the computer interface from the 2003 movie [Koi Mil Gaya](https://en.wikipedia.org/wiki/Koi..._Mil_Gaya)
+
+**Works only on Chrome/Opera**
+
+<a href="http://arjun.ninja/jadoo">![jadoo screenshot](http://arjun.ninja/blog/images/jadoo.png)</a>
--- js/crt.js
+++ js/crt.js
@@ -8,18 +8,30 @@ function onLoad(arg) {
 
 function toggleFullScreen() {
   if (!document.fullscreenElement) {
-    document.getElementById('monitor').webkitRequestFullscreen();
+    // document.getElementById('monitor').webkitRequestFullscreen();
 
-    document.querySelector('#monitor').style.backgroundColor="#161913";
-    const nodisplays = document.querySelectorAll('.nodisplay');
-    nodisplays.forEach(nodisplay => nodisplay.classList.remove('nodisplay'));
-
-    document.querySelector('.enter-text').classList.add('nodisplay');
-    document.querySelector('.background-image').classList.add('nodisplay');
-
-    const promptKey = document.querySelector('.prompt-key');
-    promptKey.innerHTML = "B C F E";
-    promptKey.innerHTML += "\tB C E D";
-    promptKey.innerHTML += "\tB C F E D C"
+    function launchIntoFullscreen(element) {
+      if(element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if(element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if(element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if(element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    }
+    launchIntoFullscreen(document.documentElement);
   }
+  document.querySelector('#monitor').style.backgroundColor="#161913";
+  const nodisplays = document.querySelectorAll('.nodisplay');
+  nodisplays.forEach(nodisplay => nodisplay.classList.remove('nodisplay'));
+
+  document.querySelector('.enter-text').classList.add('nodisplay');
+  document.querySelector('.background-image').classList.add('nodisplay');
+
+  const promptKey = document.querySelector('.prompt-key');
+  promptKey.innerHTML = "B C F E";
+  promptKey.innerHTML += "\tB C E D";
+  promptKey.innerHTML += "\tB C F E D C";
 }
